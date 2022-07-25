0871816693308aee675ddb9ea2b362ccccb32c5d
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2022-07-26 01:40:16 +0500

    feat: formik auto-animate

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { useEffect } from 'react'
 import { useStore } from './store'
 import './App.css'
@@ -17,11 +18,13 @@ function SomeoneIsTyping() {
   const someoneIsTyping = others.some((user) => user.presence?.isTyping)
 
   return someoneIsTyping ? (
-    <div className="someone_is_typing">Someon is typing</div>
+    <div className="someone_is_typing">Someone is typing</div>
   ) : null
 }
 
 function App() {
+  const [listParent] = useAutoAnimate()
+
   const {
     liveblocks: { enterRoom, leaveRoom, isStorageLoading },
     draft,
@@ -41,9 +44,9 @@ function App() {
     }
   }, [])
 
-  if (isStorageLoading) {
-    return <div>Loading...</div>
-  }
+  // if (isStorageLoading) {
+  //   return <div>Loading...</div>
+  // }
 
   return (
     <div className="container">
@@ -53,6 +56,7 @@ function App() {
         type="text"
         placeholder="What needs to be done?"
         value={draft}
+        disabled={isStorageLoading}
         onChange={(e) => setDraft(e.target.value)}
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
@@ -61,21 +65,23 @@ function App() {
         }}
       ></input>
       <SomeoneIsTyping />
-      {todos.map((todo, index) => {
-        return (
-          <div className="todo_container" key={index}>
-            <div className="todo">{todo.text}</div>
-            <button
-              className="delete_button"
-              onClick={() => {
-                deleteTodo(index)
-              }}
-            >
-              ✕
-            </button>
-          </div>
-        )
-      })}
+      <ul ref={listParent}>
+        {todos.map((todo, index) => {
+          return (
+            <li className="todo_container" key={index}>
+              <div className="todo">{todo.text}</div>
+              <button
+                className="delete_button"
+                onClick={() => {
+                  deleteTodo(index)
+                }}
+              >
+                ✕
+              </button>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
